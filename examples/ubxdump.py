b32f3cfb8d518148eddcc8d693399637f745959f
semuadmin <semuadmin@users.noreply.github.com>
2021-08-21 09:22:50 +0100

    minor updates to ubxdump utility

--- examples/ubxdump.py
+++ examples/ubxdump.py
@@ -2,14 +2,14 @@
 Simple command line utility to stream the parsed UBX output of a u-blox GNSS device.
 
 Usage (all args are optional):
-ubxdump.py port="/dev/ttyACM1" baud=9600 timeout=5 ubx_only=0 raw=0
+ubxdump.py port="/dev/ttyACM1" baud=9600 timeout=5 ubxonly=0 validate=1 raw=0
 
 If ubxonly=True (1), streaming will terminate on any non-UBX data (e.g. NMEA).
 """
 
 import sys
 from serial import Serial
-from pyubx2 import UBXReader
+from pyubx2 import UBXReader, GET, VALCKSUM
 
 # Default port settings - amend as required
 PORT = "/dev/ttyACM1"
@@ -27,13 +27,14 @@ def stream_ubx(**kwargs):
         baud = int(kwargs.get("baud", BAUD))
         timeout = int(kwargs.get("timeout", TIMEOUT))
         ubxonly = int(kwargs.get("ubxonly", 0))
+        validate = int(kwargs.get("validate", VALCKSUM))
         rawformat = int(kwargs.get("raw", 0))
         print(
             f"\nStreaming from {port} at {baud} baud in",
             f"{'raw' if rawformat else 'parsed'} format...\n",
         )
         stream = Serial(port, baud, timeout=timeout)
-        ubr = UBXReader(stream, ubxonly=ubxonly)
+        ubr = UBXReader(stream, ubxonly=ubxonly, validate=validate, msgmode=GET)
         for (raw, parsed) in ubr:
             if rawformat:
                 print(raw)
@@ -52,7 +53,7 @@ if __name__ == "__main__":
                 "the parsed UBX output of a u-blox GNSS device.\n\n",
                 "Usage (all args are optional): ubxdump.py",
                 f"port={PORT} baud={BAUD} timeout={TIMEOUT}",
-                "ubxonly=0 raw=0\n\n Type Ctrl-C to terminate.",
+                "ubxonly=0 validate=1 raw=0\n\n Type Ctrl-C to terminate.",
             )
             sys.exit()
 
