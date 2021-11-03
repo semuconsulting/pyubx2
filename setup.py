d2566870d3b49e0ee76809c9c72ca2fa9d7ca2e9
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2021-11-03 19:40:36 +0000

    add Python 3.10 to GitHUb Actions

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
         "Topic :: Utilities",
         "Topic :: Software Development :: Libraries :: Python Modules",
