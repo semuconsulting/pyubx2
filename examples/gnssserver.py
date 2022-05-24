476587548d2b9ee748b17e302e5c44d0da5870c7
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-05-24 15:47:55 +0100

    update gnssserver example

--- examples/gnssserver.py
+++ examples/gnssserver.py
@@ -7,22 +7,31 @@ a connected GNSS receiver and broadcasts the data to any
 TCP socket or NTRIP client running on a local or remote
 machine (assuming firewalls allow the hostip:port).
 
+Settings can be entered as optional command line arguments, e.g.
+> python3 gnssserver.py hostip=192.168.0.20 inport="/dev/tty.usbmodem14101" outport=6000
+Any arguments not provided will be defaulted;
+default hostip = 0.0.0.0 (i.e. binds to all available host IP address)
+default inport = "/dev/ttyACM1",
+default outport = 50010.
+
 Press CTRL-C to stop the server.
 
-The clients must be capable of parsing binary GNSS data.
+In the default configuration ('format=FORMAT_BINARY'), the clients
+must be capable of parsing binary GNSS data.
 Suitable clients include (but are not limited to):
 1) pyubx2's gnssdump cli utility invoked thus:
-   >>> gnssdump socket=hostip:50010
+   > gnssdump socket=hostip:outport
 2) The PyGPSClient GUI application, invoked thus:
-   >>> pygpsclient
+   > pygpsclient
 
-To run in NTRIP Server mode, the receiver should be an
-RTK-capable receiver (e.g. u-blox ZED-F9P) running in
-"Base Station" mode (either SURVEY_IN or FIXED). The clients
-must be NTRIP clients (e.g. PyGPSClient's NTRIP Client
-facility). FYI it'll happily run in NTRIP mode with any data
-stream but the data would be useless to an NTRIP client
-if it's not proper RTCM3 RTK correction data.
+To run in NTRIP Server mode, set 'ntripmode=1'. For this mode
+to function properly, the receiver must be an RTK-capable receiver
+(e.g. u-blox ZED-F9P) running in "Base Station" mode (either
+SURVEY_IN or FIXED). The clients must be NTRIP clients (e.g.
+PyGPSClient's NTRIP Client facility).
+NTRIP server login credentials are set via environment
+variables PYGPSCLIENT_USER and PYGPSCLIENT_PASSWORD.
+
 
 The example essentially runs two (pseudo-)concurrent threads:
 - an input thread based on the pyubx2cli.gnssdump.GNSSStreamer
@@ -42,9 +51,10 @@ Created on 24 May 2022
 """
 # pylint: disable=too-many-arguments
 
+import sys
 from queue import Queue
 from threading import Thread
-from pyubx2cli.gnssdump import GNSSStreamer, FORMAT_BINARY
+from pyubx2cli import GNSSStreamer, FORMAT_BINARY
 
 # If you have PyGPSClient installed, you can import the SocketServer classes from this...
 # from pygpsclient.socket_server import SocketServer, ClientHandler
@@ -58,8 +68,10 @@ class GNSSStatus:
     GNSS Status class.
     Container for the latest readings from the GNSS receiver.
 
-    THIS IS JUST A STUB FOR THIS EXAMPLE; IN THE ACTUAL PYGPSCLIENT
-    IMPLEMENTATION IT STORES THE LATEST LIVE GNSS READINGS
+    THIS IS JUST A STUB FOR THIS EXAMPLE; In a full-blown
+    NTRIP server, the lat/lon can be used to populate the mountpoint
+    coordinates in the sourcetable. You can set arbitrary fixed
+    values rather than 0.0 here if you prefer.
     """
 
     # pylint: disable=too-few-public-methods
@@ -78,32 +90,52 @@ class GNSSServer:
     GNSS Server Class.
     """
 
-    def __init__(
-        self, ntripmode: int, hostip: str, inport: str, outport: int, maxclients: int
-    ):
+    def __init__(self, **kwargs):
         """
         Constructor.
         """
 
-        self._ntripmode = ntripmode  # 0 = TCP Socket server mode, 1 = NTRIP server mode
-        self._hostip = hostip
-        self._inport = inport
-        self._outport = outport
-        self._msgqueue = Queue()
-        self._maxclients = maxclients
-        self._socket_server = None
-        self._clients = 0
+        try:
 
-        self.gnss_status = GNSSStatus()  # stub for compatibility with SocketServer
+            self._kwargs = kwargs
+            # overrideable command line arguments..
+            # 0 = TCP Socket Server mode, 1 = NTRIP Server mode
+            self._kwargs["ntripmode"] = int(kwargs.get("ntripmode", 0))
+            # 0.0.0.0 binds to all host IP addresses
+            self._kwargs["hostip"] = kwargs.get("hostip", "0.0.0.0")
+            # amend default as required
+            self._kwargs["port"] = kwargs.get("inport", "/dev/ttyACM1")
+            self._kwargs["outport"] = int(kwargs.get("outport", 50010))
+            # 5 is an arbitrary limit; could be significantly higher
+            self._kwargs["maxclients"] = int(kwargs.get("maxclients", 5))
 
-    def run(self):
+            # required fixed arguments...
+            self._kwargs["format"] = FORMAT_BINARY
+            msgqueue = Queue()
+            self._kwargs["ubxhandler"] = msgqueue
+            self._kwargs["nmeahandler"] = msgqueue
+            self._kwargs["rtcmhandler"] = msgqueue
+            self._socket_server = None
+            self._clients = 0
+            self._validargs = True
+
+            self.gnss_status = GNSSStatus()  # stub for compatibility with SocketServer
+
+        except ValueError as err:
+            print(f"Invalid input arguments {kwargs}\n{err}")
+            self._validargs = False
+
+    def run(self) -> int:
         """
         Run server.
         """
 
-        print("Starting server (type CTRL-C to stop)...")
-        self.start_input_thread()
-        self.start_output_thread()
+        if self._validargs:
+            print("Starting server, waiting for clients (type CTRL-C to stop)...")
+            self.start_input_thread(**self._kwargs)
+            self.start_output_thread(**self._kwargs)
+            return 1
+        return 0
 
     def stop(self):
         """
@@ -114,80 +146,67 @@ class GNSSServer:
         if self._socket_server is not None:
             self._socket_server.shutdown()
 
-    def start_input_thread(self):
+    def start_input_thread(self, **kwargs):
         """
         Start input (read) thread.
         """
 
-        print("Starting input reader thread...")
+        print("Starting input thread...")
         thread = Thread(
             target=self._input_thread,
-            args=(
-                self._inport,
-                self._msgqueue,
-            ),
+            args=(kwargs,),
             daemon=True,
         )
         thread.start()
 
-    def start_output_thread(self):
+    def start_output_thread(self, **kwargs):
         """
         Start output (socket) thread.
         """
 
-        print("Starting socket server thread...")
+        print("Starting output thread...")
         thread = Thread(
             target=self._output_thread,
             args=(
                 self,
-                self._ntripmode,
-                self._hostip,
-                self._outport,
-                self._maxclients,
-                self._msgqueue,
+                kwargs,
             ),
             daemon=True,
         )
         thread.start()
 
-    def _input_thread(self, serialport: str, msgqueue: Queue):
+    def _input_thread(self, kwargs):
         """
         THREADED
 
-        Input (read) thread.
+        Input (Serial reader) thread.
         """
 
-        # NB: any of the permissible gnssdump kwargs could be used here
-        # to configure the data that is actually placed on the message
-        # queue. e.g. you could set protfilter=1 to only output NMEA data.
-        kwargs = {
-            "port": serialport,
-            "ubxhandler": msgqueue,
-            "nmeahandler": msgqueue,
-            "rtcmhandler": msgqueue,
-            "format": FORMAT_BINARY,
-        }
+        # FYI: any of the permissible gnssdump kwargs could be passed here
+        # from command line arguments to configure the data that is
+        # actually broadcast to the clients, e.g. you could set
+        # 'protfilter=4' to only output RTCM data, or 'msgfilter=NAV-PVT'
+        # to only output UBX NAV-PVT messages.
+        # type gnssdump -h for help.
+
         gns = GNSSStreamer(**kwargs)
         gns.run()
 
-    def _output_thread(
-        self,
-        app: object,
-        ntripmode: int,
-        host: str,
-        port: int,
-        maxclients: int,
-        msgqueue: Queue,
-    ):
+    def _output_thread(self, app: object, kwargs):
         """
         THREADED
 
-        Output (socket) thread.
+        Output (socket server) thread.
         """
 
         try:
             with SocketServer(
-                app, ntripmode, maxclients, msgqueue, (host, port), ClientHandler
+                app,
+                kwargs["ntripmode"],
+                kwargs["maxclients"],
+                kwargs["ubxhandler"],
+                (kwargs["hostip"], kwargs["outport"]),
+                ClientHandler,
             ) as self._socket_server:
                 self._socket_server.serve_forever()
         except OSError as err:
@@ -196,7 +215,7 @@ class GNSSServer:
     def update_clients(self, clients: int):
         """
         Prints status message showing number of connected clients.
-        The SocketServer class invokes this method (if it exists)
+        The SocketServer object invokes this method (if implemented)
         whenever a client connects or disconnects.
 
         :param int clients: no of connected clients
@@ -215,25 +234,12 @@ def main():
     CLI Entry point.
     """
 
-    ntripmode = 0  # 0 = TCP Socket Server mode, 1 = NTRIP Server mode
-    hostip = "0.0.0.0"  # binds to all host IP addresses
-    inport = "/dev/tty.usbmodem141301"  # amend as required
-    outport = 50010  # amend as required
-    maxclients = 5  # arbitrary limit; could be significantly higher
-
     try:
 
-        server = GNSSServer(ntripmode, hostip, inport, outport, maxclients)
-        server.run()
+        server = GNSSServer(**dict(arg.split("=") for arg in sys.argv[1:]))
+        goodtogo = server.run()
 
-        while True:  # run until user presses CTRL-C
-            # While the server is running, you can connect from up to
-            # (maxclients) TCP clients running on local or remote machines
-            # (assuming the selected port is not blocked by firewalls).
-            #
-            # Suitable TCP clients include (but are not limited to)
-            # pyubx2's gnssdump cli utility or the PyGPSCLient GUI
-            # socket client.
+        while goodtogo:  # run until user presses CTRL-C
             pass
 
     except KeyboardInterrupt:
