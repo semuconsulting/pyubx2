9477ebce0f8435fab95216d44402bebeeb968b3b
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-12-30 09:25:06 +0000

    update min pyrtcm version to 1.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests", "examples", "docs"]),
     install_requires=[
         "pynmeagps>=1.0.17",
-        "pyrtcm>=1.0.0",
+        "pyrtcm>=1.0.1",
     ],
     license="BSD 3-Clause 'Modified' License",
     keywords="pyubx2 GNSS GPS GLONASS UBX NMEA GIS u-blox",
