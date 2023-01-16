5ca65791d798f3d6d8fa709a80bb0922e9c18a9b
semuadmin <28569967+semuadmin@users.noreply.github.com>
2023-01-16 19:07:47 +0000

    update utilities example

---
+++ examples/__init__.py
@@ -0,0 +1,7 @@
+"""
+Created on 27 Sep 2020
+
+:author: semuadmin
+:copyright: SEMU Consulting © 2020
+:license: BSD 3-Clause
+"""
--- examples/utilities.py
+++ examples/utilities.py
@@ -11,7 +11,7 @@ Created on 14 Jan 2023
 """
 
 from pyubx2 import latlon2dms, latlon2dmm, ecef2llh, llh2ecef, haversine
-from pyubx2.ubxtypes_core import GRS80_SMAJ_AXIS, GRS80_FLATTENING
+from DATUMS import DATUMS  # assumes this is in same folder
 
 LAT1, LON1 = 53.24, -2.16
 LAT2, LON2 = 53.32, -2.08
@@ -25,15 +25,10 @@ lat, lon = latlon2dmm(LAT2, LON2)
 print(f"D.MM = {lat}, {lon}")
 
 print(
-    f"\nFind spherical distance between {LAT1}, {LON1} and {LAT2}, {LON2} using default WGS84 datum..."
+    f"\nFind spherical distance between {LAT1}, {LON1} and",
+    f" {LAT2}, {LON2} using default WGS84 datum...",
 )
 dist = haversine(LAT1, LON1, LAT2, LON2)
-print(f"Distance: {dist/1000} km")
-
-print(
-    f"\nFind spherical distance between {LAT1}, {LON1} and {LAT2}, {LON2} using alternate GRS80 datum..."
-)
-dist = haversine(LAT1, LON1, LAT2, LON2, GRS80_SMAJ_AXIS / 1000)
 print(f"Distance: {dist/1000} km")
 
 X, Y, Z = 3822566.3113, -144427.5123, 5086857.1208
@@ -45,16 +40,28 @@ print(f"\nConvert geodetic {lat}, {lon}, {height} back to ECEF ...")
 x, y, z = llh2ecef(lat, lon, height)
 print(f"ECEF X: {x}, Y: {y}, Z: {z}")
 
+# Refer to DATUMS.py in the /examples folder for list of common
+# international datums with semi-major axis, flattening and
+# delta_x,y,z reference values
 
-# Refer to DATUMS.py for list of common international datums with
-# semi-major axis and flattening values and delta_x,y,z
+DATUM = "North_America_83"
+datum_dict = DATUMS[DATUM]
+ellipsoid, a, f, delta_x, delta_y, delta_z = datum_dict.values()
 
 print(
-    f"\nConvert ECEF X: {X}, Y: {Y}, Z: {Z} to geodetic using alternate GRS80 datum ..."
+    f"\nFind spherical distance between {LAT1}, {LON1} and",
+    f"{LAT2}, {LON2} using alternate {DATUM} ({ellipsoid}) datum...",
 )
-lat, lon, height = ecef2llh(X, Y, Z, GRS80_SMAJ_AXIS, GRS80_FLATTENING)
+dist = haversine(LAT1, LON1, LAT2, LON2, a / 1000)
+print(f"Distance: {dist/1000} km")
+
+print(
+    f"\nConvert ECEF X: {X}, Y: {Y}, Z: {Z} to",
+    f"geodetic using alternate {DATUM} ({ellipsoid}) datum ...",
+)
+lat, lon, height = ecef2llh(X - delta_x, Y - delta_z, Z - delta_z, a, f)
 print(f"Geodetic lat: {lat}, lon: {lon}, height: {height}")
 
 print(f"\nConvert geodetic {lat}, {lon}, {height} back to ECEF ...")
-x, y, z = llh2ecef(lat, lon, height, GRS80_SMAJ_AXIS, GRS80_FLATTENING)
-print(f"ECEF X: {x}, Y: {y}, Z: {z}")
+x, y, z = llh2ecef(lat, lon, height, a, f)
+print(f"ECEF X: {x + delta_x}, Y: {y + delta_y}, Z: {z + delta_z}")
