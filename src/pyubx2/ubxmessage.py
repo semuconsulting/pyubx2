f9c7b87aeb604caab12bd5baa0a15cd1e6b2ca9c
semuadmin <28569967+semuadmin@users.noreply.github.com>
2023-04-28 09:22:32 +0100

    enhance CFG-VALSET parsing

--- src/pyubx2/ubxmessage.py
+++ src/pyubx2/ubxmessage.py
@@ -186,12 +186,11 @@ class UBXMessage:
 
         index.append(0)  # add a (nested) group index
         numr, attd = att  # number of repeats, attribute dictionary
-        # if CFG-VALGET message, use dedicated method to
+        # if CFG-VALGET or CFG-VALSET message, use dedicated method to
         # parse as configuration key value pairs
-        if (
-            self._ubxClass == b"\x06"
-            and self._ubxID == b"\x8b"
-            and self._mode == ubt.GET
+        if self._ubxClass == b"\x06" and (
+            (self._ubxID == b"\x8b" and self._mode == ubt.GET)
+            or (self._ubxID == b"\x8a" and self._mode == ubt.SET)
         ):
             self._set_attribute_cfgval(offset, **kwargs)
         else:
@@ -443,7 +442,7 @@ class UBXMessage:
 
     def _set_attribute_cfgval(self, offset: int, **kwargs):
         """
-        Parse CFG-VALGET payload to set of configuration
+        Parse CFG-VALGET / CFG-VALSET payload to set of configuration
         key value pairs.
 
         :param int offset: payload offset
