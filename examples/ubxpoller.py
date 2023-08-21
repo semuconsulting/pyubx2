46be1c5e0259f3dcaee30867c3d34e843a2769b7
semuadmin <28569967+semuadmin@users.noreply.github.com>
2023-08-21 19:04:03 +0100

    update ubxpoller example

--- examples/ubxpoller.py
+++ examples/ubxpoller.py
@@ -2,13 +2,13 @@
 ubxpoller.py
 
 This example illustrates how to read, write and display UBX messages
-concurrently using threads and queues. This represents a useful
+"concurrently" using threads and queues. This represents a useful
 generic pattern for many end user applications.
 
-It implements three threads which run concurrently:
-1) a read thread which continuously reads UBX data from the receiver.
-2) a write thread which sends UBX commands or polls to the receiver.
-3) a display thread which displays parsed UBX data at the terminal.
+It implements two threads which run concurrently:
+1) a read/send thread which continuously reads UBX data from the
+receiver and sends any queued outbound command or poll messages.
+2) a display thread which displays parsed UBX data at the terminal.
 UBX data is passed between threads using queues.
 
 Press CTRL-C to terminate.
@@ -35,49 +35,42 @@ from sys import platform
 from threading import Event, Lock, Thread
 from time import sleep
 from serial import Serial
-from pyubx2 import POLL, UBX_PROTOCOL, UBXMessage, UBXReader
+from pyubx2 import POLL, UBX_PROTOCOL, UBXMessage, UBXReader, UBX_PAYLOADS_POLL
 
 
-def read_data(
+def read_send_data(
     stream: object,
     ubr: UBXReader,
-    queue: Queue,
-    lock: Lock,
+    readqueue: Queue,
+    sendqueue: Queue,
     stop: Event,
 ):
     """
     THREADED
-    Read and parse incoming UBX data and place
-    raw and parsed data on queue
+    Read and parse inbound UBX data and place
+    raw and parsed data on queue.
+
+    Send any queued outbound messages to receiver.
     """
-    # pylint: disable=unused-variable, broad-except
+    # pylint: disable=broad-exception-caught
 
     while not stop.is_set():
         if stream.in_waiting:
             try:
-                lock.acquire()
                 (raw_data, parsed_data) = ubr.read()
-                lock.release()
                 if parsed_data:
-                    queue.put((raw_data, parsed_data))
+                    readqueue.put((raw_data, parsed_data))
+
+                # refine this if outbound message rates exceed inbound
+                while not sendqueue.empty():
+                    data = sendqueue.get(False)
+                    if data is not None:
+                        ubr.datastream.write(data.serialize())
+                    sendqueue.task_done()
+
             except Exception as err:
                 print(f"\n\nSomething went wrong {err}\n\n")
                 continue
-
-
-def write_data(stream: object, queue: Queue, lock: Lock, stop: Event):
-    """
-    THREADED
-    Read queue and send UBX message to device
-    """
-
-    while not stop.is_set():
-        if queue.empty() is False:
-            message = queue.get()
-            lock.acquire()
-            stream.write(message.serialize())
-            lock.release()
-            queue.task_done()
 
 
 def display_data(queue: Queue, stop: Event):
@@ -85,11 +78,10 @@ def display_data(queue: Queue, stop: Event):
     THREADED
     Get UBX data from queue and display.
     """
-    # pylint: disable=unused-variable,
 
     while not stop.is_set():
         if queue.empty() is False:
-            (raw, parsed) = queue.get()
+            (_, parsed) = queue.get()
             print(parsed)
             queue.task_done()
 
@@ -99,11 +91,13 @@ if __name__ == "__main__":
     if platform == "win32":  # Windows
         port = "COM13"
     elif platform == "darwin":  # MacOS
-        port = "/dev/tty.usbmodem2101"
+        port = "/dev/tty.usbmodem1101"
     else:  # Linux
         port = "/dev/ttyACM1"
-    baudrate = 9600
+    baudrate = 38400
     timeout = 0.1
+
+    DELAY = 1
 
     with Serial(port, baudrate, timeout=timeout) as serial_stream:
         ubxreader = UBXReader(serial_stream, protfilter=UBX_PROTOCOL)
@@ -114,21 +108,12 @@ if __name__ == "__main__":
         stop_event = Event()
 
         read_thread = Thread(
-            target=read_data,
+            target=read_send_data,
             args=(
                 serial_stream,
                 ubxreader,
                 read_queue,
-                serial_lock,
-                stop_event,
-            ),
-        )
-        write_thread = Thread(
-            target=write_data,
-            args=(
-                serial_stream,
                 send_queue,
-                serial_lock,
                 stop_event,
             ),
         )
@@ -142,19 +127,25 @@ if __name__ == "__main__":
 
         print("\nStarting handler threads. Press Ctrl-C to terminate...")
         read_thread.start()
-        write_thread.start()
         display_thread.start()
 
         # loop until user presses Ctrl-C
         while not stop_event.is_set():
             try:
                 # DO STUFF IN THE BACKGROUND...
-                # poll the receiver port configuration using CFG-PRT
-                print("\nPolling port configuration CFG-PRT...\n")
-                for prt in (0, 1, 2, 3, 4):  # I2C, UART1, UART2, USB, SPI
-                    msg = UBXMessage("CFG", "CFG-PRT", POLL, portID=prt)
-                    send_queue.put(msg)
-                sleep(1)
+                # poll all available NAV messages (receiver will only respond
+                # to those NAV message types it supports; responses won't
+                # necessarily arrive in sequence)
+                count = 0
+                for nam in UBX_PAYLOADS_POLL:
+                    if nam[0:4] == "NAV-":
+                        print(f"Polling {nam} message type...")
+                        msg = UBXMessage("NAV", nam, POLL)
+                        send_queue.put(msg)
+                        count += 1
+                        sleep(DELAY)
+                stop_event.set()
+                print(f"{count} NAV message types polled.")
 
             except KeyboardInterrupt:  # capture Ctrl-C
                 print("\n\nTerminated by user.")
@@ -162,6 +153,5 @@ if __name__ == "__main__":
 
         print("\nStop signal set. Waiting for threads to complete...")
         read_thread.join()
-        write_thread.join()
         display_thread.join()
         print("\nProcessing complete")
