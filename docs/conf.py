aca89c6a2f64c07697cd4e978126de371a4abc76
semuadmin <28569967+semuadmin@users.noreply.github.com>
2024-10-10 19:15:58 +0100

    add readthedocs support

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,20 @@ html_title = "<project> v<version> documentation."
 html_static_path = ["_static"]
 html_last_updated_fmt = "%b %d %Y"
 html_theme_options = {
-    "display_version": True,
+    "logo_only": False,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "vcs_pageview_mode": "",
+    "style_nav_header_background": "white",
+    "flyout_display": "hidden",
+    "version_selector": True,
+    "language_selector": True,
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
 }
 
 autodoc_default_options = {
