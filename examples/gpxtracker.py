ddce6aeb4a8b74ed9b9c0d902050d116f2b04a2d
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2025-08-03 08:57:17 +0100

    update gpxtracker example

--- examples/gpxtracker.py
+++ examples/gpxtracker.py
@@ -3,6 +3,10 @@ Simple CLI utility which creates a GPX track file
 from a binary UBX dump (such as that created by
 PyGPSClient's datalogging facility) using pyubx2.UBXReader().
 
+Usage:
+
+python3 gpxtracker.py
+
 Dump must contain UBX NAV-PVT, NAV-POSLLH or NAV-HPPOSLLH messages.
 If the message doesn't include an explicit date, the utility will use
 today's date in conjunction with the message iTOW.
@@ -55,78 +59,68 @@ class UBXTracker:
         self._ubxreader = None
         self._connected = False
 
-    def open(self):
-        """
-        Open datalog file.
-        """
-
-        self._infile = open(self._filename, "rb")
-        self._connected = True
-
-    def close(self):
-        """
-        Close datalog file.
-        """
-
-        if self._connected and self._infile:
-            self._infile.close()
-
-    def reader(self):
+    def run(self):
         """
         Reads and parses UBX message data from stream
         using UBXReader iterator method
         """
 
         i = 0
-        self._ubxreader = UBXReader(self._infile, validate=VALCKSUM)
+        print(f"\nProcessing file {self._filename}...")
 
-        self.write_gpx_hdr()
+        with open(self._filename, "rb") as infile:
 
-        for _, msg in self._ubxreader:
-            try:
-                if msg.identity == "NAV-PVT":
-                    time = (
-                        datetime(
-                            msg.year, msg.month, msg.day, msg.hour, msg.min, msg.second
-                        ).isoformat()
-                        + "Z"
-                    )
-                    if msg.fixType == 3:
-                        fix = "3d"
-                    elif msg.fixType == 2:
-                        fix = "2d"
-                    else:
-                        fix = "none"
-                    self.write_gpx_trkpnt(
-                        msg.lat,
-                        msg.lon,
-                        ele=msg.hMSL / 1000,  # height in meters
-                        time=time,
-                        fix=fix,
-                    )
-                if msg.identity in ("NAV-POSLLH", "NAV-HPPOSLLH"):
-                    time = (
-                        date.today().isoformat()
-                        + "T"
-                        + itow2utc(msg.iTOW).isoformat()
-                        + "Z"
-                    )
-                    self.write_gpx_trkpnt(
-                        msg.lat,
-                        msg.lon,
-                        ele=msg.hMSL / 1000,  # height in meters
-                        time=time,
-                    )
+            self._connected = True
+            self._ubxreader = UBXReader(infile, validate=VALCKSUM)
 
-                i += 1
-            except (ube.UBXMessageError, ube.UBXTypeError, ube.UBXParseError) as err:
-                print(f"Something went wrong {err}")
-                continue
+            self.write_gpx_hdr()
 
-        self.write_gpx_tlr()
+            for _, msg in self._ubxreader:
+                try:
+                    if msg.identity == "NAV-PVT":
+                        time = (
+                            datetime(
+                                msg.year, msg.month, msg.day, msg.hour, msg.min, msg.second
+                            ).isoformat()
+                            + "Z"
+                        )
+                        if msg.fixType == 3:
+                            fix = "3d"
+                        elif msg.fixType == 2:
+                            fix = "2d"
+                        else:
+                            fix = "none"
+                        self.write_gpx_trkpnt(
+                            msg.lat,
+                            msg.lon,
+                            ele=msg.hMSL / 1000,  # height in meters
+                            time=time,
+                            fix=fix,
+                        )
+                    if msg.identity in ("NAV-POSLLH", "NAV-HPPOSLLH"):
+                        time = (
+                            date.today().isoformat()
+                            + "T"
+                            + itow2utc(msg.iTOW).isoformat()
+                            + "Z"
+                        )
+                        self.write_gpx_trkpnt(
+                            msg.lat,
+                            msg.lon,
+                            ele=msg.hMSL / 1000,  # height in meters
+                            time=time,
+                        )
+
+                    i += 1
+                except (ube.UBXMessageError, ube.UBXTypeError, ube.UBXParseError) as err:
+                    print(f"Something went wrong {err}")
+                    continue
+
+            self.write_gpx_tlr()
 
         print(f"\n{i} NAV message{'' if i == 1 else 's'} read from {self._filename}")
         print(f"{i} trackpoint{'' if i == 1 else 's'} written to {self._trkfname}")
+        print("\nOperation Complete")
 
     def write_gpx_hdr(self):
         """
@@ -135,13 +129,13 @@ class UBXTracker:
 
         timestamp = strftime("%Y%m%d%H%M%S")
         self._trkfname = os.path.join(self._outdir, f"gpxtrack-{timestamp}.gpx")
-        self._trkfile = open(self._trkfname, "a")
+        self._trkfile = open(self._trkfname, "a", encoding="utf-8")
 
-        date = datetime.now().isoformat() + "Z"
+        dat = datetime.now().isoformat() + "Z"
         gpxtrack = (
             XML_HDR + "<gpx " + GPX_NS + ">"
             f"<metadata>"
-            f'<link href="{GITHUB_LINK}"><text>pyubx2</text></link><time>{date}</time>'
+            f'<link href="{GITHUB_LINK}"><text>pyubx2</text></link><time>{dat}</time>'
             "</metadata>"
             "<trk><name>GPX track from UBX NAV-PVT datalog</name><trkseg>"
         )
@@ -201,8 +195,4 @@ if __name__ == "__main__":
     infilep = input("Enter input UBX datalog file: ").strip('"')
     outdirp = input("Enter output directory: ").strip('"')
     tkr = UBXTracker(infilep, outdirp)
-    print(f"\nProcessing file {infilep}...")
-    tkr.open()
-    tkr.reader()
-    tkr.close()
-    print("\nOperation Complete")
+    tkr.run()
