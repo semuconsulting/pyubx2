816d1a4a95b3978a1324652f4953fd4d02cc40a5
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2025-09-05 23:49:43 +0100

    update example

--- examples/ubx_spatialite.py
+++ examples/ubx_spatialite.py
@@ -4,7 +4,7 @@ ubx_spatialite.py
 Simple example script illustrating how to write parsed UBX GNSS data to
 a sqlite3/spatialite database previously created in QGIS.
 
-gnssdata table has the following fields:
+gnssdata table in gnss.db has the following fields:
     pk integer
     geom POINT
     source text
@@ -26,11 +26,13 @@ from os import environ, path
 
 from pyubx2 import ERR_LOG, UBX_PROTOCOL, UBXReader
 
+# input file containing binary UBX data
 INFILE = "pygpsdata-ubx.log"
+# path to spatialite database created using QGIS
 DBPATH = path.join("/home/myuser/Downloads/qgis", "gnss.db")
-SLPATH = (
-    "/opt/QGIS_3.44.1/bin"  # amend path to mod_spatialite.so module in QGIS binaries
-)
+# path to mod_spatialite.so (or .dll) module in QGIS binaries
+SLPATH = "/opt/QGIS_3.44.1/bin"
+# INSERT SQL statement
 SQLI1 = (
     "INSERT INTO gnssdata (source, time, fixtype, difftype, dop, hacc, geom) "
     "VALUES ('{}', {}, {}, {}, {}, {}, GeomFromText('POINT({} {})', 4326));"
@@ -42,7 +44,7 @@ environ["PATH"] = SLPATH + ";" + environ["PATH"]
 # connect to the database
 con = sqlite3.connect(DBPATH)
 cur = con.cursor()
-# load spatialite extension to support spatial attributes
+# load spatialite extension to support spatial (geometry) attributes
 con.enable_load_extension(True)
 con.load_extension("mod_spatialite")
 # con.execute("SELECT InitSpatialMetaData();") # already done by QGIS
